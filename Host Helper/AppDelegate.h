27af07aa047459a8ca1292fa7354d38142fd3db0
Willy-Wonka <willwcc@gmail.com>
2015-05-09 21:32:56 -0700

    Initial Commit

---
+++ Host Helper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Host Helper
+//
+//  Created by Will on 2015-05-09.
+//  Copyright (c) 2015 Muscle Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Host Helper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Host Helper
+//
+//  Created by Will on 2015-05-09.
+//  Copyright (c) 2015 Muscle Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
